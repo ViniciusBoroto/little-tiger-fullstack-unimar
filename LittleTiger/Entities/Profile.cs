135f46b06f5526718b45680a9e0969a46ef1e062
hspszmatheus <hspszmatheus@gmail.com>
2026-03-30 22:26:11 -0300

    feat: validation for profile

--- LittleTiger/Entities/Profile.cs
+++ LittleTiger/Entities/Profile.cs
@@ -1,15 +1,28 @@
-﻿namespace LittleTiger.Entities
+﻿using LittleTiger.ValueObjects;
+
+namespace LittleTiger.Entities
 {
     public class Profile : EntityBase
     {
         public List<Achievement> Achivements { get; private set; }
-        public decimal WonAmount { get; private set; }
+        public Amount WonAmount { get; private set; } //quantidade de lucro 
         public int PlacedBets { get; private set; }
 
         public Profile()
         {
-            
+
+        }
+
+        //TODO: métodos para aumentar  contador (placedbets) e o lucro (WonAmount)
+
+        public void IncreaseAmount(Amount wonAmount)
+        {
+            this.WonAmount += wonAmount;
+        }
+        public void InceasePlacedBets()
+        {
+            this.PlacedBets += 1;
         }
     }
-   
+
 }
