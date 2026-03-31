67ac4b22103a2d2116914143c85ffea68c9e2db6
hspszmatheus <hspszmatheus@gmail.com>
2026-03-30 22:02:13 -0300

    update: profile

--- LittleTiger/Entities/Profile.cs
+++ LittleTiger/Entities/Profile.cs
@@ -3,6 +3,8 @@
     public class Profile : EntityBase
     {
         public List<Achievement> Achivements { get; private set; }
+        public decimal WonAmount { get; private set; }
+        public int PlacedBets { get; private set; }
 
         public Profile()
         {
--- LittleTiger/Entities/UserHistory.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace LittleTiger.Entities
-{
-    public class UserHistory : EntityBase
-    {
-        public decimal WonAmount { get; private set; }
-        public int PlacedBets { get; private set; }
-    }
-}
