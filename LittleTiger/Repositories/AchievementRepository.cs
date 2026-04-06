304a81a6d5494d09d34adc3228e43759a849595e
hspszmatheus <hspszmatheus@gmail.com>
2026-04-06 20:23:33 -0300

    feat: repository user and achievements

---
+++ LittleTiger/Repositories/AchievementRepository.cs
@@ -0,0 +1,25 @@
+﻿using LittleTiger.Interfaces;
+using LittleTiger.Entities;
+
+namespace LittleTiger.Repositories
+{
+    public class AchievementRepository : IAchievementRepository
+    {
+        private static List<Achievement> _achievement = new List<Achievement>();
+
+        public List<Achievement> GetAll()
+        {
+            return _achievement;
+        }
+
+        public void Create(Achievement conquest)
+        {
+            if (conquest == null)
+            {
+                throw new Exception("Nao foi possivel adicionar a conquista");
+            }
+            _achievement.Add(conquest);
+            return "deu certo";
+        }
+    }
+}
---
+++ LittleTiger/Repositories/UserRepository.cs
@@ -0,0 +1,22 @@
+﻿using LittleTiger.Contexts.Auth.Entities;
+using LittleTiger.Contexts.Auth.Repositories;
+using LittleTiger.Entities;
+
+namespace LittleTiger.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private static List<User> _users = new List<User> ();
+
+        public void AddAchievementToUser(User user, Achievement achieveme)
+        {
+            if (achieveme == null)
+            {
+                throw new Exception("vix deu erro man...");
+            }
+            _users.Add(user);
+
+
+        }
+    }
+}
