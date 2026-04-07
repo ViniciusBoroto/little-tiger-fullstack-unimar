d4c105b3a2d2f9d73bcf8756625716e97e84a86a
Jeniffer Scarpini <j.scarpiniquintam@gmail.com>
2026-04-06 22:10:57 -0300

    CreateUseCase

--- LittleTiger/Entities/Achievement.cs
+++ LittleTiger/Entities/Achievement.cs
@@ -2,6 +2,15 @@
 {
     public class Achievement : EntityBase
     {
+        private Guid guid;
+
+        public Achievement(Guid guid, string name, string description)
+        {
+            this.guid = guid;
+            Name = name;
+            Description = description;
+        }
+
         public string Name { get; private set; }
         public string Description { get; private set; }
         public List<User> Users { get; set; }
--- LittleTiger/UseCases/CreateAchievementUseCase.cs
+++ LittleTiger/UseCases/CreateAchievementUseCase.cs
@@ -1,6 +1,40 @@
-﻿namespace LittleTiger.UseCases
+﻿using System;
+using LittleTiger.DTOs;
+using LittleTiger.Entities;
+using LittleTiger.Interfaces;
+
+namespace LittleTiger.UseCases
 {
-    public class AchievementUseCase
+    public class CreateAchievementUseCase : IAchievementUseCase
     {
+        private readonly IAchievementRepository _achievementRepository;
+
+        public CreateAchievementUseCase(IAchievementRepository achievementRepository)
+        {
+            _achievementRepository = achievementRepository;
+        }
+
+        public Achievement Run(CreateAchievementRequest request)
+        {
+            // 1. Validação de Contrato (Input)
+            if (request == null) throw new ArgumentNullException(nameof(request));
+
+     
+
+            // 3. Criação da Entidade
+            // É importante que o construtor da sua classe Achievement valide as regras internas
+            // (ex: pontos não podem ser negativos)
+            var achievement = new Achievement(
+                Guid.NewGuid(),
+                request.Name,
+                request.Description
+            );
+
+            // 4. Persistência
+            _achievementRepository.Create(achievement);
+
+            // 5. Retorno
+            return achievement;
+        }
     }
 }
